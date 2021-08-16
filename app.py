d5dda262dc2d016f6d62f98cd6fa5fcd9cc07460
sevmac <sivam.evt@gmail.com>
2021-08-16 21:26:42 +0300

    Added html templates

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 'First page'
+    return render_template('index.html')
 
 
-@app.route('/about')
-def aboutPage():
-    return 'This is About page'
+@app.route('/users')
+def about_page():
+    return render_template('users.html')
+
+
+@app.route('/users/<name>')
+def user_page(name):
+    return render_template('profile_page.html', name=name)
+
+
+@app.route('/urls')
+def urlss():
+    return str(app.url_map)
 
 
 if __name__ == '__main__':
