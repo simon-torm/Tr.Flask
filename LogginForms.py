8d3f41c3cd109984f901640e9bea90f0f8b69281
sevmac <sivam.evt@gmail.com>
2021-08-27 23:14:48 +0300

    Added authentication

--- LogginForms.py
+++ LogginForms.py
@@ -1,15 +1,24 @@
 import flask_wtf
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email
 from wtforms.fields.html5 import DateField
 
 class LogginForm(flask_wtf.FlaskForm):
     name = StringField("Name: ", validators=[DataRequired()])
     password = PasswordField("Password: ")
+    remember_me = BooleanField("Remember me")
     submit = SubmitField("Submit")
 
 
-class NameCookie(flask_wtf.FlaskForm):
+class RegistrationForm(flask_wtf.FlaskForm):
+    login = StringField("Login/email:", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    password = PasswordField("Password: ", validators=[DataRequired()])
+    repeat_password = PasswordField("Repeat password: ", validators=[DataRequired()])
+    registration_submit = SubmitField("Registration")
+
+
+class NameCookieForm(flask_wtf.FlaskForm):
     name = StringField("Your name: ", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
@@ -19,8 +28,13 @@ class NewPlanForm(flask_wtf.FlaskForm):
     submit = SubmitField("Add plan")
 
 
-class NewPlanElement(flask_wtf.FlaskForm):
-    date = DateField("Date", validators=[DataRequired()], format='%Y-%m-%d')
+class NewPlanElementForm(flask_wtf.FlaskForm):
+    date = DateField("Date")
     text = StringField("Text", validators=[DataRequired()])
     materials = StringField("Materials")
     submit = SubmitField("Add plan")
+
+
+class NewNameForm(flask_wtf.FlaskForm):
+    new_name = StringField("New name: ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 
 from flask import Flask, render_template, request, redirect, url_for, flash, make_response, session
-from LogginForms import LogginForm, NameCookie, NewPlanForm, NewPlanElement
+from LogginForms import LogginForm, NameCookieForm, NewPlanForm, NewPlanElementForm, \
+    RegistrationForm, NewNameForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, login_user, logout_user,login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy as sa
 from datetime import datetime
 
@@ -14,9 +17,18 @@ app.debug = True
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message = "Please log in to access this page"
+login_manager.login_message_category = 'warning'
 
 kargs_base_template = dict()
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    # print(user_id)
+    return db.session.query(User).get(user_id)
 
 @app.before_request
 def set_session():
@@ -37,49 +49,112 @@ def index():
     return render_template('index.html', **kargs_base_template)
 
 
-@app.route('/users', methods=['GET', 'POST'])
-def users():
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
     form = LogginForm()
-    if form.validate_on_submit():
+    registration_form = RegistrationForm()
+
+    if form.submit.data and form.validate_on_submit():
+        print("Logging in")
         name = form.name.data
         password = form.password.data
-        print('Data input: ')
-        print(name)
-        print(password)
-        flash("You were successfully logged in", "success")
-        return redirect('/')
+        # print('Data input: ')
+        # print(name)
+        # print(password)
+        user = db.session.query(User).filter(User.login == name).first()
+        if user and user.check_password(password):
+            login_user(user, remember=form.remember_me.data)
+            flash("You were successfully logged in", 'success')
+            return  redirect('/')
+        else:
+            flash("Invalid username/password", 'danger')
+            return redirect('/login')
 
-    return render_template('users.html', form=form, **kargs_base_template)
+    if registration_form.registration_submit.data and registration_form.validate_on_submit():
+        print("register")
+        login = registration_form.login.data
+        name = registration_form.name.data
+        password = registration_form.password.data
+        repeat_password = registration_form.repeat_password.data
+
+        if db.session.query(User).filter(User.login == login).first():
+            flash("This login is already taken", 'danger')
+            return redirect('/login')
+        elif not password == repeat_password:
+            flash("Passwords do not match", 'danger')
+            return redirect('/login')
+        elif login and name and password:
+            new_user = User()
+            new_user.login = login
+            new_user.name = name
+            new_user.set_password(password)
+            db.session.add(new_user)
+            db.session.commit()
+            print("New user added")
+            flash("You have successfully registered", 'success')
+            return redirect('/login')
+
+    return render_template('login_page.html',
+                           form=form,
+                           registration_form=registration_form,
+                           **kargs_base_template)
+
+
+@app.route('/user', methods=['GET', 'POST'])
+@login_required
+def user_page():
+    form = NewNameForm()
+    if form.validate_on_submit():
+        current_user.name = form.new_name.data
+        db.session.commit()
+        flash("You have successfully changed your name", 'success')
+        return redirect('/user')
+
+    return render_template('user_page.html',
+                           user=current_user,
+                           form=form,
+                           **kargs_base_template)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have successfully logged out', 'success')
+    return redirect('/')
 
 
 @app.route('/cookie', methods=['GET', 'POST'])
 def set_cookie_page():
-    form = NameCookie()
+    form = NameCookieForm()
     if form.validate_on_submit():
         name = form.name.data
 
         res = make_response(redirect('/'))
         res.set_cookie('name', name, max_age=60)
-        flash("You were successfully set a cookies")
+        flash("You were successfully set a cookies", "success")
 
         return res
 
 
     return render_template('cookie.html', form=form, **kargs_base_template)
 
+
 @app.route('/plans', methods=['GET', 'POST'])
+@login_required
 def plans_page():
     form = NewPlanForm()
     if form.validate_on_submit():
         plan_name = form.name.data
         new_plan = Plan(title=plan_name)
+        new_plan.user_id = current_user.id
         db.session.add(new_plan)
         db.session.commit()
         print("Added new plan")
-        flash(f"\"{plan_name}\" added!")
+        flash(f"\"{plan_name}\" added!", 'success')
         return redirect('/plans')
 
-    plans = db.session.query(Plan).all()[::-1] # DESC
+    plans = db.session.query(Plan).filter(Plan.user_id == current_user.id).all()[::-1] # DESC
     return render_template('plans_page.html',
                            form=form,
                            plan_list = plans,
@@ -87,22 +162,38 @@ def plans_page():
 
 
 @app.route('/plan/<int:id>', methods=['GET', 'POST'])
+@login_required
 def plan_page(id):
-    form = NewPlanElement()
     if not bool(db.session.query(Plan).get(id)):
         return "This plan does not exist!", 404
 
+
+    if not db.session.query(Plan).get(id).user_id == current_user.id:
+        flash("You do not have access to this plan", 'danger')
+        return redirect('/plans')
+
+    form = NewPlanElementForm()
+    print(form.errors)
     if form.validate_on_submit():
+        print("!!!")
         date = form.date.data
         text = form.text.data
         materials = form.materials.data
 
-        element = ListElement(plan_id=id, date=date, text=text, materials=materials)
+        element = ListElement()
+        element.plan_id = id
+        element.text = text
+        if date:
+            element.date = date
+        if materials:
+            element.materials = materials
+        print(materials)
+
         db.session.add(element)
         db.session.commit()
 
         print("Added new element")
-        flash(f"Added new element!")
+        flash(f"Added new element!", "success")
         return redirect(f"/plan/{id}")
 
 
@@ -114,9 +205,10 @@ def plan_page(id):
                            **kargs_base_template)
 
 
-@app.route('/users/<name>')
-def user_page(name):
-    return render_template('profile_page.html', name=name, **kargs_base_template)
+@app.route('/user/<name>')
+@login_required
+def user_name_page(name):
+    return redirect('/user')
 
 
 @app.route('/urls')
@@ -127,24 +219,32 @@ def urlss():
 
 # Table models
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "Users"
     id = sa.Column(sa.Integer, primary_key=True)
     login = sa.Column(sa.String(80), unique=True, nullable=False)
     name = sa.Column(sa.String(80), nullable=False)
+    password_hash = sa.Column(sa.String(102), nullable=False)
     created_on = sa.Column(sa.DateTime(), default=datetime.utcnow)
+    admin = sa.Column(sa.Boolean(), default=False)
 
-    plans = db.relationship("Plan", backref="user", cascade="all,delete-orphan")
+    plans = db.relationship("Plan", backref="user")
 
     def __repr__(self):
         return f"({self.id}) {self.login}: {self.name}"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Plan(db.Model):
     __tablename__ = "Plans"
     id = sa.Column(sa.Integer, primary_key=True)
     title = sa.Column(sa.String(80), unique=True, nullable=False)
-    user_id = sa.Column(sa.Integer, db.ForeignKey('Users.id'))
+    user_id = sa.Column(sa.Integer, db.ForeignKey('Users.id', ondelete="SET NULL"))
     created_on = sa.Column(sa.DateTime(), default=datetime.utcnow)
     updated_on = sa.Column(sa.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
 
